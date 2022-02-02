c0ce752d6a87dc6088b2aaf63b8b7b8c5e015e7e
Janez Troha <dz0ny@ubuntu.si>
2022-02-02 18:14:37 +0100

    Add wasm interop

---
+++ go.mod
@@ -0,0 +1,5 @@
+module aremykeyssafe
+
+go 1.16
+
+require golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"crypto/rsa"
+	"syscall/js"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func getSSHKeyLength(this js.Value, args []js.Value) interface{} {
+	key := inputs[0].String()
+	callback := inputs[len(inputs)-1:][0]
+	parsedKey, _, _, _, err := ssh.ParseAuthorizedKey([]byte(key))
+	if err != nil {
+		callback.Invoke(err, js.Null())
+	}
+	// To get back to an *rsa.PublicKey, we need to first upgrade to the
+	// ssh.CryptoPublicKey interface
+	parsedCryptoKey := parsedKey.(ssh.CryptoPublicKey)
+
+	// Then, we can call CryptoPublicKey() to get the actual crypto.PublicKey
+	pubCrypto := parsedCryptoKey.CryptoPublicKey()
+
+	// Finally, we can convert back to an *rsa.PublicKey
+	pub := pubCrypto.(*rsa.PublicKey)
+
+	
+	callback.Invoke(js.Null(), pub.N.BitLen())
+}
+
+func main() {
+	block := make(chan bool)
+    js.Global().Set("getSSHKeyLength", js.FuncOf(getSSHKeyLength))
+	<-block
+}
