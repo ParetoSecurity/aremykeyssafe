9cfdb100f995623789f803402dcfb78e7df321af
Janez Troha <dz0ny@ubuntu.si>
2022-04-25 11:08:25 +0200

    Add checker for GPG public keys

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module aremykeyssafe
 
 go 1.16
 
-require golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838
+require (
+	github.com/ProtonMail/gopenpgp/v2 v2.4.6
+	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838
+)
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"aremykeyssafe/ssh"
 	"syscall/js"
+
+	"github.com/ProtonMail/gopenpgp/v2/crypto"
 )
 
 func getSSHKeyLength(this js.Value, args []js.Value) interface{} {
@@ -16,8 +18,20 @@ func getSSHKeyLength(this js.Value, args []js.Value) interface{} {
 	return nil
 }
 
+func getGPGExpired(this js.Value, args []js.Value) interface{} {
+	pubKey := args[0].String()
+	callback := args[len(args)-1:][0]
+	gpgKey, err := crypto.NewKeyFromArmored(pubKey)
+	if err != nil {
+		callback.Invoke(err, js.Null(), js.Null())
+	}
+	callback.Invoke(js.Null(), gpgKey.IsExpired())
+	return nil
+}
+
 func main() {
 	block := make(chan bool)
 	js.Global().Set("getSSHKeyLength", js.FuncOf(getSSHKeyLength))
+	js.Global().Set("getGPGExpired", js.FuncOf(getGPGExpired))
 	<-block
 }
